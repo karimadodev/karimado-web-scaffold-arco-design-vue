f25b832ed7e624d5327568886b63b21c6d77ab3c
John Doe <johndoe@example.com>
2023-12-29 00:17:12 +0800

    feat: Change imagemin plugin

--- web/config/plugins/imagemin.ts
+++ web/config/plugins/imagemin.ts
@@ -1,36 +1,10 @@
 /**
- * Image resource files used to compress the output of the production environment.
+ * Optimize your image assets using Sharp.js and SVGO.
  *
- * https://github.com/anncwb/vite-plugin-imagemin
+ * https://github.com/FatehAK/vite-plugin-image-optimizer
  */
-import imageminPlugin from 'vite-plugin-imagemin';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default function configImageminPlugin() {
-  return imageminPlugin({
-    gifsicle: {
-      optimizationLevel: 7,
-      interlaced: false,
-    },
-    optipng: {
-      optimizationLevel: 7,
-    },
-    mozjpeg: {
-      quality: 20,
-    },
-    pngquant: {
-      quality: [0.8, 0.9],
-      speed: 4,
-    },
-    svgo: {
-      plugins: [
-        {
-          name: 'removeViewBox',
-        },
-        {
-          name: 'removeEmptyAttrs',
-          active: false,
-        },
-      ],
-    },
-  });
+  return ViteImageOptimizer();
 }
